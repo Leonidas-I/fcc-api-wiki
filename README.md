a177766c730e8f355be470d421be7f42e5abaa02
Leonidas-I <morningstar8x@outlook.com>
2019-11-23 22:08:19 +0700

    bundle splitting and fix minor bug

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ use font awesome 5 and jquery event on it
 get rid of unused bootstrap in production css file  
 prefixer for generated css  
 source map js and css for dev mode  
+bundle splitting work fine  
 
 # Things i may improve  
-code splitting still working  
+working on code splitting dynamic import  
+
--- webpack.common.js
+++ webpack.common.js
@@ -2,34 +2,28 @@ module.exports = {
   entry: {
     main: './src/main.js'
   },
-  // optimization: {
-  //   splitChunks: {
-  //     cacheGroups: {
-  //       jquery: {
-  //         test: /[\\/]node_modules[\\/]jquery[\\/]/,
-  //         name: 'jquery',
-  //         chunks: 'all'
-  //       },
-  //       bootstrap: {
-  //         test: /[\\/]node_modules[\\/]bootstrap[\\/]/,
-  //         name: 'bootstrap',
-  //         chunks: 'all'
-  //       },
-  //       common: {
-  //         test: /[\\/]node_modules[\\/](popper.js|@fortawesome)[\\/]/,
-  //         name: 'common',
-  //         chunks: 'all'
-  //       },
-  //       styles: {
-  //         name: 'styles',
-  //         test: /\.css$/,
-  //         chunks: 'all',
-  //         enforce: true
-  //       }
-  //     },
-  //     chunks: 'all'
-  //   }
-  // },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/](jquery|popper.js)[\\/]/,
+          name: 'vendor',
+          chunks: 'all'
+        },
+        common: {
+          test: /[\\/]node_modules[\\/](bootstrap|@fortawesome)[\\/]/,
+          name: 'common',
+          chunks: 'all'
+        },
+        styles: {//why i comment out this but still purge unused bootstrap
+          name: 'styles',
+          test: /\.css$/,
+          chunks: 'all',
+          enforce: true
+        }
+      }
+    }
+  },
   module: {
     rules: [
       {
